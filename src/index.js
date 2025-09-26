f583aa0c9d0367d27bbbe4112904760af8fd42d8
LukaDelicc <ldelic762@gmail.com>
2025-09-26 11:32:42 +0200

    Create backend skeleton

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { getDB, queryDB } from "./db/db";
+import { User, Quiz, Question, Category, Result } from "./db/classes";
 
 export default {
 	async fetch(request, env, _ctx) {
@@ -26,7 +27,6 @@ export default {
 				}
 				try {
 					const db = getDB(env);
-					const { User } = await import("./db/classes.js");
 					const data = await request.json();
 					const user = new User(data);
 					const result = await user.create(db);
@@ -45,6 +45,160 @@ export default {
 					return new Response(`Error: ${e.message}`, { status: 500 });
 				}
 			}
+			case "/quiz/new": {
+				if (request.method !== "POST") {
+					return new Response("Method Not Allowed", { status: 405 });
+				}
+				try {
+					const db = getDB(env);
+					const { Quiz } = await import("./db/classes.js");
+					const data = await request.json();
+					const quizData = {
+						name: data.name || 'Placeholder name',
+						score_needed: data.score_needed || 'Placeholder name',
+						max_time: data.max_time || 'Placeholder name'
+					}
+					const quiz = new Quiz(quizData);
+
+					const result = await quiz.create(db);
+					if (result.success) {
+						return new Response(
+							JSON.stringify({ success: true, user_id: user.id }),
+							{ headers: { "Content-Type": "application/json" } },
+						);
+					} else {
+						return new Response(
+							JSON.stringify({ success: false, error: "User creation failed" }),
+							{ headers: { "Content-Type": "application/json" }, status: 500 },
+						);
+					}
+
+
+
+
+				} catch (e) {
+					return new Response(`Error: ${e.message}`, { status: 500 });
+				}
+
+
+			}
+			case "/quiz/question": {
+				if (request.method !== "POST") {
+					return new Response("Method Not Allowed", { status: 405 });
+				}
+				try {
+					const db = getDB(env);
+					const { Question } = await import("./db/classes.js");
+					const data = await request.json();
+					const questionData = {
+						text: data.text || 'Placeholder',
+						country: data.country || 'Placeholder country',
+						difficulty: data.difficulty || 'Placeholder',
+						score_multiplier: data.score_multiplier || 'Placeholder'
+
+					}
+					const question = new Question(questionData);
+
+					const result = await question.create(db);
+					if (result.success) {
+						return new Response(
+							JSON.stringify({ success: true, user_id: user.id }),
+							{ headers: { "Content-Type": "application/json" } },
+						);
+					} else {
+						return new Response(
+							JSON.stringify({ success: false, error: "User creation failed" }),
+							{ headers: { "Content-Type": "application/json" }, status: 500 },
+						);
+
+					}
+
+				} catch (e) {
+					return new Response(`Error: ${e.message}`, {
+						status: 500
+					});
+				}
+			}
+			case "/quiz/category": {
+				if (request.method !== "POST") {
+					return new Response("Method Not Allowed", { status: 405 });
+				}
+				try {
+					const db = getDB(env);
+					const { Category } = await import("./db/classes.js");
+					const data = await request.json();
+					const categoryData = {
+						create_date: data.create_date || 'Placeholder',
+						update_date: data.update_date || 'Placeholder'
+
+
+					}
+					const category = new Category(categoryData);
+
+					const result = await category.create(db);
+					if (result.success) {
+						return new Response(
+							JSON.stringify({ success: true, user_id: user.id }),
+							{ headers: { "Content-Type": "application/json" } },
+						);
+					} else {
+						return new Response(
+							JSON.stringify({ success: false, error: "User creation failed" }),
+							{ headers: { "Content-Type": "application/json" }, status: 500 },
+						);
+
+					}
+
+				}
+				catch (e) {
+					return new Response(`Error: ${e.message}`, {
+						status: 500
+					});
+				}
+			}
+			case "/quiz/result": {
+				if (request.method !== "POST") {
+					return new Response("Method Not Allowed", { status: 405 });
+				}
+				try {
+					const db = getDB(env);
+					const { Result } = await import("./db/classes.js");
+					const data = await request.json();
+					const resultData = {
+						score: data.score || 'Placeholder',
+						time_taken: data.time_taken || 'Placeholder',
+						create_date: data.create_date || 'Placeholder',
+						update_date: data.update_date || 'Placeholder'
+					}
+					const resultObject = new Result(resultdata);
+
+					const result = await result.create(db);
+					if (result.success) {
+						return new Response(
+							JSON.stringify({ success: true, user_id: user.id }),
+							{ headers: { "Content-Type": "application/json" } },
+						);
+					} else {
+						return new Response(
+							JSON.stringify({ success: false, error: "User creation failed" }),
+							{ headers: { "Content-Type": "application/json" }, status: 500 },
+						);
+
+					}
+
+				} catch (e) {
+					return new Response(`Error: ${e.message}`, {
+						status: 500
+					});
+
+				}
+			}
+
+
+
+
+
+
 
 			default:
 				return new Response("Not Found", { status: 404 });
