900f6388b2f9f896a58101b2fc0a491fcfb5e1c9
famous <105926828+dezegozer@users.noreply.github.com>
2025-09-30 11:46:50 +0200

    add test route back

--- src/index.js
+++ src/index.js
@@ -184,6 +184,93 @@ export default {
 				}
 			}
 
+			// simple test route to create and test all classes, teacher wanted it :P ignore the formatting that messed up with merging w main
+			case "/test-db": {
+				try {
+					const db = getDB(env);
+					console.log("Creating user...");
+					return_msg += "Creating user...\n";
+					const newUser = new User({
+						email: "bV5yaKxsxxs@example.com",
+						name: "Test User",
+						password: "password",
+					});
+					await newUser.create(db);
+					console.log("Loading user...");
+					return_msg += "Loading user...\n";
+					const user = new User();
+					await user.load(db, 1);
+					console.log(user);
+					console.log("Creating category...");
+					return_msg += "Creating category...\n";
+					const newCategory = new Category({ name: "Culture" });
+					await newCategory.create(db);
+					console.log("Loading category...");
+					return_msg += "Loading category...\n";
+					const category = new Category();
+					await category.load(db, 1);
+					console.log(category);
+					console.log("Creating quiz...");
+					return_msg += "Creating quiz...\n";
+					const newQuiz = new Quiz({
+						name: "Test Quiz",
+						description: "This is a test quiz",
+						score_needed: 5,
+						max_time: 30,
+					});
+					await newQuiz.create(db);
+					console.log("Loading quiz...");
+					return_msg += "Loading quiz...\n";
+					const quiz = new Quiz();
+					await quiz.load(db, 1);
+					console.log(quiz);
+					console.log("Creating question...");
+					return_msg += "Creating question...\n";
+					const newQuestion = new Question({
+						quiz_id: 1,
+						category_id: 1,
+						text: "What is the capital of Croatia?",
+					});
+					await newQuestion.create(db);
+					console.log("Loading question...");
+					return_msg += "Loading question...\n";
+					const question = new Question();
+					await question.load(db, 1);
+					console.log(question);
+					console.log("Creating option...");
+					return_msg += "Creating option...\n";
+					const newOption = new Option({
+						question_id: 1,
+						option: "Zagreb",
+						correct: true,
+					});
+					await newOption.create(db);
+					console.log("Loading option...");
+					return_msg += "Loading option...\n";
+					const option = new Option();
+					await option.load(db, 1);
+					console.log(option);
+					console.log("Creating result...");
+					return_msg += "Creating result...\n";
+					const newResult = new Result({
+						user_id: 1,
+						quiz_id: 1,
+						score: 10,
+						time_taken: 25,
+					});
+					await newResult.create(db);
+					console.log("Loading result...");
+					return_msg += "Loading result...\n";
+					const result = new Result();
+					await result.load(db, 1);
+					console.log(result);
+					return new Response(`${return_msg}Done!`);
+				} catch (e) {
+					return new Response(`Error: ${e.message}`, {
+						status: 500,
+					});
+				}
+			}
 			default:
 				return new Response("Not Found", { status: 404 });
 		}
